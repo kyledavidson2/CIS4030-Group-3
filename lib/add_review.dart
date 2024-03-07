509b59462b9c00a80d2af091f4d79f386dbb9365
Jose <jcortez@uoguelph.ca>
2024-03-07 14:04:31 -0500

    Fixed add review page

--- lib/add_review.dart
+++ lib/add_review.dart
@@ -10,24 +10,35 @@ class AddReview extends StatelessWidget{
     return Scaffold(
       appBar: AppBar(title: const Text("ADD REVIEW"),backgroundColor: Theme.of(context).colorScheme.inversePrimary,centerTitle: true,),
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Center(child: const Text("Choose a rating:")),
-          Icon(Icons.star_rate),
-          const Text("Enter Details:"),
-          SizedBox(
-            width: 200,
-            child: TextField(
-              autofocus: true,
-              decoration: InputDecoration(
-                enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black),borderRadius: BorderRadius.circular(10)),
-                contentPadding: EdgeInsets.all(2)
+          Column(
+            children: [
+              Center(child: const Text("Choose a rating:")),
+              Icon(Icons.star_rate),
+            ],
+          ),
+          Column(
+            children: [
+              const Text("Enter Details:"),
+              SizedBox(
+                width: 200,
+                child: TextField(
+                  autofocus: false,
+                  decoration: InputDecoration(
+                    enabledBorder: OutlineInputBorder(borderSide: BorderSide(color: Colors.black),borderRadius: BorderRadius.circular(10)),
+                    contentPadding: EdgeInsets.symmetric(vertical: 40)
+                  ),
+                
+                ),
               ),
-            
-            ),
-          )
+            ],
+          ),
+          AddPhotosButton(),
 
         ],
       ),
+      bottomNavigationBar: MainNavigationBar(),
     );
   }
 }
